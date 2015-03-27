7ac5398de782f76429576e229cfa57410e6f5e9b
Andrey Nering <andrey.nering@gmail.com>
2015-03-27 16:07:07 -0300

    Added table_for helper

--- lib/simple_table_for.rb
+++ lib/simple_table_for.rb
@@ -1,2 +1,41 @@
 module SimpleTableFor
+  def field(content, options = {})
+    <<-TD.html_safe
+      <td id='#{options[:id]}' class='#{options[:class]}'>
+        #{content}
+      </td>
+    TD
+  end
+
+  # Usage:
+  #   <%= table_for @posts, %w[Title Text Date Comments\ count -] do |post| %>
+  #     <%= field post.title %>
+  #     <%= field post.text %>
+  #     <%= field post.date %>
+  #     <%= field post.comments.count %>
+  #     <%= field link_to('View', post) %>
+  #   <% end %>
+  def table_for(collection, heads, options = {})
+    heads = heads.map{|h| "<th>#{h}</th>".html_safe }.join('')
+
+    rows = collection.map do |obj|
+      "<tr>#{capture{ yield obj }}</tr>".html_safe
+    end.join('')
+
+    <<-TABLE.html_safe
+      <table id='#{options[:id]}' class='#{options[:class]}'>
+        <thead>
+          <tr>
+            #{heads}
+          </tr>
+        </thead>
+
+        <tbody>
+          #{rows}
+        </tbody>
+      </table>
+    TABLE
+  end
 end
+
+ActionView::Base.send :include, SimpleTableFor if defined? ActionView::Base
--- test/simple_table_for_test.rb
+++ test/simple_table_for_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class SimpleTableForTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SimpleTableFor
-  end
 end
