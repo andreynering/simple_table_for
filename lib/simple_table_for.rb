2378878433eea8ebe728010027bd7db208cb502d
Andrey Nering <andrey.nering@gmail.com>
2015-03-30 11:13:39 -0300

    Moving helpers to its own module

--- lib/simple_table_for.rb
+++ lib/simple_table_for.rb
@@ -1,41 +1,60 @@
 module SimpleTableFor
-  def field(content, options = {})
-    <<-TD.html_safe
-      <td id='#{options[:id]}' class='#{options[:class]}'>
-        #{content}
-      </td>
-    TD
+  module Helpers
+    def field(content, options = {})
+      <<-TD.html_safe
+        <td id='#{options[:id]}' class='#{options[:class]}'>
+          #{content}
+        </td>
+      TD
+    end
+
+    # Usage:
+    #   <%= table_for @posts, %w[Title Text Date Comments\ count -] do |post| %>
+    #     <%= field post.title %>
+    #     <%= field post.text %>
+    #     <%= field post.date %>
+    #     <%= field post.comments.count %>
+    #     <%= field link_to('View', post) %>
+    #   <% end %>
+    def table_for(collection, heads, options = {})
+      heads = heads.map{|h| "<th>#{h}</th>".html_safe }.join('')
+
+      rows = collection.map do |obj|
+        "<tr>#{capture{ yield obj }}</tr>".html_safe
+      end.join('')
+
+      options = Defaults.get.merge options
+
+      <<-TABLE.html_safe
+        <table id='#{options[:id]}' class='#{options[:class]}'>
+          <thead>
+            <tr>
+              #{heads}
+            </tr>
+          </thead>
+
+          <tbody>
+            #{rows}
+          </tbody>
+        </table>
+      TABLE
+    end
   end
 
-  # Usage:
-  #   <%= table_for @posts, %w[Title Text Date Comments\ count -] do |post| %>
-  #     <%= field post.title %>
-  #     <%= field post.text %>
-  #     <%= field post.date %>
-  #     <%= field post.comments.count %>
-  #     <%= field link_to('View', post) %>
-  #   <% end %>
-  def table_for(collection, heads, options = {})
-    heads = heads.map{|h| "<th>#{h}</th>".html_safe }.join('')
+  class Defaults
+    def self.get
+      @defaults || defaults
+    end
 
-    rows = collection.map do |obj|
-      "<tr>#{capture{ yield obj }}</tr>".html_safe
-    end.join('')
+    def self.set(options)
+      @defaults = defaults.merge options
+    end
 
-    <<-TABLE.html_safe
-      <table id='#{options[:id]}' class='#{options[:class]}'>
-        <thead>
-          <tr>
-            #{heads}
-          </tr>
-        </thead>
-
-        <tbody>
-          #{rows}
-        </tbody>
-      </table>
-    TABLE
+    private
+      def self.defaults
+        {id: '', class: ''}
+      end
   end
 end
 
-ActionView::Base.send :include, SimpleTableFor if defined? ActionView::Base
+ActionView::Base.send :include, SimpleTableFor::Helpers if defined? ActionView::Base
